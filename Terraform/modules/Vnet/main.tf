5b6ec20bc3a260a5c4762478a5e4a4abbf7e88b4
Arikeru Iroanwusi <arikeru@gmail.com>
2025-03-06 19:46:45 -0500

    Update: finishing Vnet module

---
+++ Terraform/modules/Vnet/main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_virtual_machine" "vm" {
+    subscription            = var.subscription
+    name                    = var.vnet_name
+    location                = var.location
+    resource_group          = var.resource_group_name
+    address_space           = var.address_space
+    subnet                  = var.subnet
+    subnet_address_space    = var.subnet_address_space
+}
---
+++ Terraform/modules/Vnet/outputs.tf
@@ -0,0 +1,24 @@
+output "name" {
+  description = "Name of VM"
+  value       = azurerm_virtual_machine.vm.name
+}
+
+output "location" {
+  description = "Location of the VM"
+  value       = azurerm_virtual_machine.vm.location
+}
+
+output "address_space" {
+  description = "Address space of the VM"
+  value       = azurerm_virtual_machine.vm.address_space
+}
+
+output "subnet" {
+  description = "Name of the Subnet"
+  value       = azurerm_virtual_machine.vm.subnet
+}
+
+output "subnet_address" {
+  description = "Name of the Subnet"
+  value       = azurerm_virtual_machine.vm.subnet_address_space
+}
